d37cddfe37b3e64247c398ed3c13861468c2df5d
JAguVillar <jaguvillar@gmail.com>
2024-09-06 00:23:56 -0300

    avances

--- seccion_4/app.js
+++ seccion_4/app.js
@@ -1,7 +1,36 @@
 const http = require("http");
+const express = require("express");
 
-const routes = require("./routes");
+const app = express();
 
-const server = http.createServer(routes);
+//middleware basico
+//esta funcion que le pasamos a app, sera ejecutada por cada peticion o solicitud entrante
+//esta funcion recibe 3 parametros req, res, next
+//req es la solicitud entrante, res es la respuesta de la aplicacion, next es una funcion que se ejecuta despues de la funcion anterior
+// app.use((req, res, next) => {
+//   console.log("Middleware");
+//   next(); //permite ejecutar el siguiente middleware en fila
+// });
+// app.use((req, res, next) => {
+//   console.log("Middleware 2");
+//   res.send("<h1>Hola mundo</h1>"); //envia la respuesta al cliente, reemplaza a write
+// });
 
-server.listen(3000);
+//aqui usamo el middleware approach para controlar lo que queremos mostrar y el orden
+//enrutamos nuestras requests en diferentes middlewares
+
+app.use("/ruta", (req, res, next) => {
+  console.log("Middleware 2");
+  res.send("<h1>Ruta</h1>"); //envia la respuesta al cliente, reemplaza a write
+});
+app.use("/", (req, res, next) => {
+  console.log("Middleware 2");
+  res.send("<h1>Hola mundo</h1>"); //envia la respuesta al cliente, reemplaza a write
+});
+
+// const server = http.createServer(app);
+// server.listen(3000);
+
+//lo de arriba es lo mismo que abajo, solo escrito con express
+
+app.listen(3000);
